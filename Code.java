29ee158c5bbce85d0d5e2787235cadb512fe45e5
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-07-13 11:31:05 +0000

    create Code.java

---
+++ Code.java
@@ -0,0 +1,11 @@
+/** Translate differetn instruction fields into binary code
+ * 
+ * different methods invoked with the part of instruction
+ * as arguments and returns the correspoinding binary code
+ * for that field
+ * 
+ **/
+ 
+public class Code {
+    
+}
