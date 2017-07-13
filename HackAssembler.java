c77458a167dae4186c0a74ce8b1046b0438548fe
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-07-13 09:48:31 +0000

    add description to hackassembler.java

--- HackAssembler.java
+++ HackAssembler.java
@@ -1,6 +1,10 @@
-/** An Assembler for the Hack computer.
+/** An Assembler for the Hack computer(16bit computer in Nand2Tetris course).
  * 
- * Read 
+ * Handle I/O files,
+ * Open the file provided in arguments
+ * read a instruction
+ * convert it into binary code
+ * write the binary code in the output file
  **/
 
 public class HackAssembler {
