fe31ee9091ebd4817704f4b97069cefbab5bd59e
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-07-14 03:11:31 +0000

    read file in HackAssembler

--- HackAssembler.java
+++ HackAssembler.java
@@ -8,17 +8,19 @@
  **/
 
 public class HackAssembler {
-    public String file;
-    
-    public HackAssembler(String f) {
-        this.file = f;
-        // read the file and
-        // in a loop read a instruction
-        // convert it into binary
-        // write it into output file
-    }
-    
+    // read the file and
+    // in a loop read a instruction
+    // convert it into binary
+    // write it into output file
     public static void main(String[] args) {
-        
+        // read the file
+        File infile = new File(args[0]);
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		// in a loop read an instruction
+		String inst;
+		while((inst = br.readLine()) != null) {
+		    
+		}
     }
 }
