377f8c29f6d151615b696dbe9bc20bbe04ae47bc
Vipin Kumar <vipin.ind.kumar@gmail.com>
2017-07-14 08:08:28 +0000

    create API for Parser.java

--- Parser.java
+++ Parser.java
@@ -7,5 +7,40 @@
  **/
  
 public class Parser {
+    public String instruction;
     
+    // Initializes with an instruction 
+    public Parser(String inst) {
+        
+    }
+    
+    // check if it's an instruction(not white space or comment)
+    public boolean isInstruction() {
+        
+    }
+    
+    // convert the instruction into it's corresponding binary code
+    public String binary() {
+        
+    }
+    
+    // return the A-instruction's part to be converted
+    public String aInstruction() {
+        
+    }
+    
+    // return comp field
+    public String comp() {
+        
+    }
+    
+    // return dest field
+    public String dest() {
+        
+    }
+    
+    // return jump field
+    public String jump() {
+        
+    }
 }
